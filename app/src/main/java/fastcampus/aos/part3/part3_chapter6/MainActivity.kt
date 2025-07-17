d8bc99f530390c047e53c02b14f1bb1c93c391c9
ParkHyunSu99 <phs0767@pineone.com>
2025-07-17 23:54:50 +0900

    리스트 출력하기

--- app/src/main/java/fastcampus/aos/part3/part3_chapter6/MainActivity.kt
+++ app/src/main/java/fastcampus/aos/part3/part3_chapter6/MainActivity.kt
@@ -2,24 +2,38 @@ package fastcampus.aos.part3.part3_chapter6
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import dagger.hilt.android.AndroidEntryPoint
 import fastcampus.aos.part3.part3_chapter6.databinding.ActivityMainBinding
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
-    private val listAdapter by lazy { ListAdapter() }
+    private val viewModel : MainViewModel by viewModels()
+    private val adapter by lazy { PagingListAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater).apply {
             setContentView(root)
-            recyclerView.adapter = listAdapter
+            recyclerView.adapter = adapter
         }
 
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            viewModel.pagingData.collectLatest {
+                if (it != null) adapter.submitData(lifecycle, it)
+            }
+        }
     }
 }
