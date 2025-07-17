203166996ad10c86e6bde3749c22aa6263bd62c1
ParkHyunSu99 <phs0767@pineone.com>
2025-07-17 16:20:33 +0900

    화면 구성하기 1

--- app/src/main/java/fastcampus/aos/part3/part3_chapter6/MainActivity.kt
+++ app/src/main/java/fastcampus/aos/part3/part3_chapter6/MainActivity.kt
@@ -6,17 +6,18 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import dagger.hilt.android.AndroidEntryPoint
+import fastcampus.aos.part3.part3_chapter6.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
         }
+
     }
 }
